88eaf20b1baad6e7142f2ab2e317263b4c8e674e
Ionut Moraru <morion4000@gmail.com>
2020-01-14 21:02:32 +0200

    Update ERC20Factory.js

--- src/components/ERC20Factory.js
+++ src/components/ERC20Factory.js
@@ -3,19 +3,20 @@ import ERC20Artifact from '../artifacts/erc20.json';
 
 class ERC20Factory {
   constructor(web3) {
-    this.wallet = web3.currentProvider.selectedAddress;
     this.contract = new web3.eth.Contract(ERC20Artifact.abi);
-    this.contract.options.from = web3.currentProvider.selectedAddress;
-    this.contract.options.gas = 3000000;
-    this.contract.options.gasPrice = '3000000000'; // 3 Gwei
-    this.contract.options.data = ERC20Artifact.bytecode;
+    this.wallet = web3.currentProvider.selectedAddress;
   }
 
   async create(name, symbol, decimals) {
     return this.contract.deploy({
+      data: ERC20Artifact.bytecode,
       arguments: [name, symbol, decimals]
     })
-    .send()
+    .send({
+      from: this.wallet,
+      gasPrice: '3000000000', // 3 Gwei
+      gas: 3000000
+    })
     .on('error', (error) => {
       console.log(error);
     })
